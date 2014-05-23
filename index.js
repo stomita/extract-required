b6bb2b8d1cd3d65f235ed7d2252efd5cf6770de7
Shinichi Tomita <shinichi.tomita@gmail.com>
2014-05-23 14:07:27 +0900

    fix to add ./ prefix to relative path loading module

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var esquery = require('esquery');
 var fs = require('fs');
 var path = require('path');
 
-var extractRequire = module.exports = function(code, options) {
+var extractRequiredModules = module.exports = function(code, options) {
   options = options || {}
-  var cwd = options.cwd || process.cwd();
+  var cwd = options.cwd;
   var srcDir = options.src ? path.dirname(options.src) : cwd;
   var ast = esprima.parse(code);
   var matches = esquery(ast, "CallExpression[callee.name=\"require\"]");
@@ -13,7 +13,8 @@ var extractRequire = module.exports = function(code, options) {
     var p = node.arguments.length === 1 &&
             node.arguments[0].type === 'Literal' &&
             node.arguments[0].value;
-    return p[0] === "." ? path.relative(cwd, path.resolve(srcDir, p)) : p;
+    // if the module name is relative path, change to relative path from cwd.
+    return cwd && p.charAt(0) === "." ? "./" + path.relative(cwd, path.resolve(srcDir, p)) : p;
   }).filter(function(name) { return name; });
 }
 
@@ -21,7 +22,7 @@ if (require.main === module) {
   var filename = process.argv[process.argv.length - 1];
   fs.readFile(filename, 'utf-8', function(err, data) {
     if (err) { return console.error(err); }
-    var results = extractRequire(data, { src: filename, cwd: process.cwd() });
+    var results = extractRequiredModules(data, { src: filename, cwd: process.cwd() });
     console.log(results); 
   });
 }
